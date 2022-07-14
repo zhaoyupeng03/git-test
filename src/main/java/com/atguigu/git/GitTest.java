0475f6d5e254a710bf75a24cc29acc22ab8b574c
zhaoyupeng <zhaoyupeng@atguigu.com>
2022-07-14 16:44:35 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author zypstart
+ * @create 2022-07-14 16:40
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
